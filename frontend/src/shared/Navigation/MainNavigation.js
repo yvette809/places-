0f5601db5e4ad2815e77ed1944596ea4e09064ec
Yvette <nchombuayvta@gmil.com>
2020-08-27 00:54:29 +0200

    sideDrawer

--- frontend/src/shared/Navigation/MainNavigation.js
+++ frontend/src/shared/Navigation/MainNavigation.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import NavLinks from './NavLinks'
 import MainHeader from './MainHeader'
+import SideDrawer from './SideDrawer'
 
 function MainNavigation(props) {
     return (
+        <>
+        <SideDrawer>
+            <nav className= "main-navigation__drawer-nav">
+                <NavLinks/>
+            </nav>
+        </SideDrawer>
        <MainHeader>
-           <button className = "main-navigation_menu-btn">
+           <button className = "main-navigation__menu-btn">
                <span/>
                <span/>
                <span/>
            </button>
-           <h1 className= "main-navigation_title">
+           <h1 className= "main-navigation__title">
                Your places
            </h1>
-           <nav>
-               ...
+           <nav className = "main-navigation__header-nav">
+              <NavLinks/>
            </nav>
        </MainHeader>
+       </>
     )
 }
 
